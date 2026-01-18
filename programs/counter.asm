ac8a214dfc52a3c95b80cd88b9da956141b7f2c7
jamiebrn <jjbtech25@gmail.com>
2026-01-18 21:59:51 +0000

    swapping isa around trying to simplify architecture. too tired rn

--- programs/counter.asm
+++ programs/counter.asm
@@ -1,8 +1,11 @@
 
 .main:
     imm     ra      0x00
-    imm     rb      0x00
+    imm     0xE     0x00
+    imm     0xF     0x00
 .loop:
-    iadd    rb      rb      0x1
-    addc    ra      ra      ra
+    iadd    ra      ra      0x1
+    iadd    0xF     ra      0x0
+    sto     ra
     jmp     .loop
+
