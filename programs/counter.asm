a4f5889217af9cf532520877c248edfd52001b1e
jamiebrn <jjbtech25@gmail.com>
2026-01-19 21:46:01 +0000

    starting to fall into place. giving me a headache

--- programs/counter.asm
+++ programs/counter.asm
@@ -1,11 +1,11 @@
 
 .main:
-    imm     ra      0x00
-    imm     0xE     0x00
+    imm     ra      0x10
+.initloop:
+    iadd    0xF     0xF      0x0
+    sto     0xF
+    cmp     0xF     ra
+    jmpz    .double
+    jmp     .initloop
+.double:
     imm     0xF     0x00
-.loop:
-    iadd    ra      ra      0x1
-    iadd    0xF     ra      0x0
-    sto     ra
-    jmp     .loop
-
