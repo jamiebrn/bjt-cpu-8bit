16a2760ed4905fc8aa204b9420f92c4b0d79450c
jamiebrn <jjbtech25@gmail.com>
2026-01-25 20:21:39 +0000

    holy logisim working build this is crazy

--- programs/counter.asm
+++ programs/counter.asm
@@ -2,10 +2,10 @@
 .main:
     imm     ra      0x10
 .initloop:
-    iadd    0xF     0xF      0x0
+    iadd    0xF     0xF      0x01
     sto     0xF
     cmp     0xF     ra
-    jmpz    .double
+    jmpz    .end
     jmp     .initloop
-.double:
-    imm     0xF     0x00
+.end:
+    stop
